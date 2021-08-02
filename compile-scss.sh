ad1c8fe2bb5b5723cb69708d368ecef86169dd50
Gamerappa <chazpelo2@gmail.com>
2021-08-02 15:33:26 -0400

    Fix file name for BS2 theme

--- compile-scss.sh
+++ compile-scss.sh
@@ -3,9 +3,9 @@ sass scss/_bootstrap.scss dist/css/bootstrap.css
 echo "Compiling Bracketstrap - Minimal"
 sass scss/_bootstrap.scss dist/css/bootstrap.min.css --style=compressed
 echo "Compiling Bracketstrap - Theme Normal"
-sass scss/bootstrap/_theme.scss dist/css/bootstrap.theme.css
+sass scss/bootstrap/_theme.scss dist/css/bootstrap-theme.css
 echo "Compiling Bracketstrap - Theme Minimal"
-sass scss/bootstrap/_theme.scss dist/css/bootstrap.theme.min.css --style=compressed
+sass scss/bootstrap/_theme.scss dist/css/bootstrap-theme.min.css --style=compressed
 echo "Compiling Bracketstrap - Docs Normal"
 sass docs/assets/scss/docs.scss docs/assets/css/docs.css
 echo "Compiling Bracketstrap - Docs Minimal"
