dfc175efa1f096846552f957feb5e1053d8db6cf
Gamerappa <chazpelo2@gmail.com>
2021-08-02 15:02:13 -0400

    Bracketstrap SCSS Part 2

--- compile-scss.sh
+++ compile-scss.sh
@@ -3,11 +3,12 @@ sass scss/_bootstrap.scss dist/css/bootstrap.css
 echo "Compiling Bracketstrap - Minimal"
 sass scss/_bootstrap.scss dist/css/bootstrap.min.css --style=compressed
 echo "Compiling Bracketstrap - Theme Normal"
-sass scss/_theme.scss dist/css/bootstrap.theme.css
+sass scss/bootstrap/_theme.scss dist/css/bootstrap.theme.css
 echo "Compiling Bracketstrap - Theme Minimal"
-sass scss/_theme.scss dist/css/bootstrap.theme.min.css --style=compressed
+sass scss/bootstrap/_theme.scss dist/css/bootstrap.theme.min.css --style=compressed
 echo "Compiling Bracketstrap - Docs Normal"
 sass docs/assets/scss/docs.scss docs/assets/css/docs.css
 echo "Compiling Bracketstrap - Docs Minimal"
 sass docs/assets/scss/docs.scss docs/assets/css/docs.min.css --style=compressed
-cp 
+echo "Copying to Docs"
+cp dist/css/* docs/dist/css/
